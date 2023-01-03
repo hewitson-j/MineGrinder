8b5b9b7a95ea4d86ee8170a2e89adb320c3d95a5
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-01-03 16:36:17 -0700

    Added dummy text and section headings.

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ homeButton = document.getElementById("home-button");
 aboutButton = document.getElementById("author-button");
 buyButton = document.getElementById("buy-button");
 navbar = document.getElementById("navbar");
+photo = document.getElementById("test");
+pHeaders = document.querySelectorAll(".headers");
+paragraphs = document.querySelectorAll(".paragraphs");
+body = document.querySelector("body");
 
 function jumpscare() {
+  //Initial text change for main header
   header.textContent = "run.";
+
+  //Text Changes for anchor elements
   setInterval(() => {
     homeButton.textContent = "hide.";
   }, 1000);
@@ -16,6 +23,7 @@ function jumpscare() {
     buyButton.textContent = "die.";
   }, 3000);
 
+  //Changes color for items to make them "disappear"
   setInterval(() => {
     header.style.color = "black";
   }, 4000);
@@ -32,8 +40,24 @@ function jumpscare() {
     buyButton.style.color = "black";
   }, 8000);
   setInterval(() => {
-    location.reload();
+    for (var i = 0; i < pHeaders.length; i++) {
+      pHeaders[i].style.color = "black";
+    }
+  }, 9000);
+  setInterval(() => {
+    for (var i = 0; i < paragraphs.length; i++) {
+      paragraphs[i].style.color = "black";
+    }
   }, 10000);
+
+  //Shows image
+  setInterval(() => {
+    photo.style.display = "block";
+    body.style.overflow = "hidden";
+  }, 11000);
+  setInterval(() => {
+    location.reload();
+  }, 20000);
 
   console.log("What does 'die' mean?");
 }
